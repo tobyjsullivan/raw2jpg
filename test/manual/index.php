9100722c3e8c369105a7c15e5fd860163ce60440
Toby Sullivan <tobys@hootsuite.com>
2013-02-15 18:42:08 -0800

    Implemented basic skeleton for test page

---
+++ test/manual/index.php
@@ -0,0 +1,13 @@
+<?php 
+require_once('../../src/Raw2Jpg.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Testing the converter</title>
+</head>
+<body>
+	<h1>Here, we shall test the conversion!</h1>
+	<p>TODO</p>
+</body>
+</html>
