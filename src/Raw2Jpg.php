a63f3079e58c056e6188fcd545a8d7ce76033292
Toby Sullivan <tobys@hootsuite.com>
2013-02-15 19:06:59 -0800

    Added some initial test data for a couple canons

--- src/Raw2Jpg.php
+++ src/Raw2Jpg.php
@@ -1,4 +1,6 @@
 <?php
 class Raw2Jpg {
-	
+	public static function convert($rawInputPath, $jpgOutputPath) {
+// 		throw new Exception("Method not implemented");
+	}
 }
--- test/manual/index.php
+++ test/manual/index.php
@@ -8,6 +8,15 @@ require_once('../../src/Raw2Jpg.php');
 </head>
 <body>
 	<h1>Here, we shall test the conversion!</h1>
-	<p>TODO</p>
+	<h2>Canon 5D Mark II (.CR2)</h2>
+	<?php 
+	Raw2Jpg::convert('../data/5DII_TestShot_F8.CR2', 'output/5DII_TestShot_F8.jpg');
+	?>
+	<p><img src="/test/manual/output/5DII_TestShot_F8.jpg"></p>
+	<h2>Canon 1Ds Mark III (.CR2)</h2>
+	<?php 
+	Raw2Jpg::convert('../data/1DsIII_TestShot_F8.CR2', 'output/1DsIII_TestShot_F8.jpg');
+	?>
+	<p><img src="/test/manual/output/1DsIII_TestShot_F8.jpg"></p>
 </body>
 </html>
